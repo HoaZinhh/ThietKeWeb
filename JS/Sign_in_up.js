7d4cca6f800cc89d9ed652ed92f0224c16e92141
HoaZinhh <hoavuongvinh@gmail.com>
2023-07-13 16:33:40 +0700

    1.3
    Sign up-in page

---
+++ JS/Sign_in_up.js
@@ -0,0 +1,34 @@
+function signup(e){
+    event.preventDefault();
+    var username = document.getElementById(elemmentId = "username").value;
+    var email = document.getElementById(elemmentId = "email").value;
+    var password = document.getElementById(elemmentId =  "password").value;
+    var user ={
+        username:username,
+        email:email,
+        password,
+    }
+    var json = JSON.stringify(value = user);
+    localStorage.setItem(key = username,value = json);
+    alert(message = "Đăng kí thành công !!!")
+}
+
+function login(e){
+    event.preventDefault();
+    var username = document.getElementById(elemmentId = "username").value;
+    var email = document.getElementById(elemmentId = "email").value;
+    var password = document.getElementById(elemmentId =  "password").value;
+    var user = localStorage.getItem(key = username);
+    var data = JSON.parse(text = user);
+    
+    if(user == null){
+        alert(message="Vui lòng nhập Username & Password")
+    }
+    else if(username = data.username && email == data.email && password == data.password){
+        alert(message = "Đăng nhập thành công")
+        window.location.href = "index.html"
+    } 
+    else{
+        alert(message = "Đăng nhập thất bại")
+    }
+}
